3ca6b51e5031be232ab6cd9e613ccd7308c80379
Sebastian Hermida <sebastian@sbastn.com>
2011-03-07 07:31:10 -0500

    Project skeleton

---
+++ src/com/happyprog/pulse/views/ChartView.java
@@ -0,0 +1,18 @@
+package com.happyprog.pulse.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class ChartView extends ViewPart {
+
+	public static final String ID = "com.happyprog.pulse.views.ChartView";
+
+	@Override
+	public void createPartControl(Composite parent) {
+	}
+
+	@Override
+	public void setFocus() {
+	}
+
+}
